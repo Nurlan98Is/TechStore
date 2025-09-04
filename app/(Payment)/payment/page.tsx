afdde2923c98dcf63c24054503f5029395f8d65d
Nurlan Iskakov <iskakov9821@outlook.com>
2025-09-04 15:04:32 +0500

    adaptive payment page

--- app/(Payment)/payment/page.tsx
+++ app/(Payment)/payment/page.tsx
@@ -11,26 +11,28 @@ import {CardTypesInMapsFn} from "@/entities/creditCard/types/CreditCardTypes";
 
 
 export default function PaymentPage() {
-    const state = useSelector((state: RootState) => state.cart);
+    const productCartInPayment = useSelector((state: RootState) => state.cart);
     const creditCards = useSelector((state: RootState) => state.user.creditCards);
-    console.log('State type:', typeof state);
-    console.log('Is array:', Array.isArray(state));
+
     return (
-        <div className='flex justify-around mb-[72px]'>
+        <div className='flex flex-col lg:flex-row justify-around mb-[72px]'>
             <div className='xl:w-[512px] xl:h-[704px] border border-[#D1D1D8] rounded-[10px] px-[24px] py-[32px]'>
                 <p className='text-[20px] font-medium mb-[24px]'>Summary</p>
                 <div>
-                    {state.map((product) => {
-                        return (
+                    {productCartInPayment.length === 0 ? (
+                        <p>Cart is empty</p>
+                    ) : (
+                        productCartInPayment.map((product) => (
                             <ProductInPayment
+                                key={product.id}
                                 name={product.name}
                                 price={product.price}
-                                img={product.img.src}
-                                key={product.id}
+                                img={product.img?.src}
                             />
-                        );
-                    })}
+                        ))
+                    )}
                 </div>
+
                 <div>
                     <div className='flex flex-col justify-around h-[136px]'>
                         <div className='flex flex-col justify-around h-[72px]'>
@@ -48,7 +50,7 @@ export default function PaymentPage() {
                 </div>
 
             </div>
-            <div className='lx:w-[512px] xl:h-[704px]'>
+            <div className='lx:w-[512px] xl:h-[704px] mt-[24px] lg:mt-[0px] flex flex-col justify-center items-center lg:items-start'>
                 <p className='mb-[24px]'>Payment</p>
                 <div className='flex gap-[56px] mb-[32px]'>
                     <button>Credit Card</button>
@@ -56,17 +58,25 @@ export default function PaymentPage() {
                     <button>PayPal Credit</button>
                 </div>
                 <Slider>
-                    {creditCards.map((card: CardTypesInMapsFn) => (
-                        <CreditCard cardHolder={card.cardHolder} cardNumber={card.cardNumber} key={card.id}/>
-                    ))}
+                    {creditCards.length === 0 ? (
+                        <p>You need add card</p>
+                    ) : (
+                        creditCards.map((card: CardTypesInMapsFn) => (
+                            <CreditCard
+                                key={card.id}
+                                cardHolder={card.cardHolder}
+                                cardNumber={card.cardNumber}
+                            />
+                        ))
+                    )}
                 </Slider>
-                <div className='flex my-[49px]'>
+                <div className='flex justify-center lg:justify-start my-[49px]'>
                     <input type='checkbox'/>
                     <p className='pl-[5px]'>Same as billing address</p>
                 </div>
-                <div>
-                    <Button borderColor={'black'}>Back</Button>
-                    <Button borderColor={'black'} backgroundColor={'black'} textColor={'white'} className='ml-[23px]'>Pay</Button>
+                <div className='flex justify-center items-center lg:flex-row lg:justify-start'>
+                    <Button borderColor={'black'} width={156}>Back</Button>
+                    <Button borderColor={'black'} width={156} backgroundColor={'black'} textColor={'white'} className='ml-[23px]'>Pay</Button>
                 </div>
             </div>
         </div>
